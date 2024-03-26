4c18120c1a5cdf1b5fea98aaf45180d47be92e9a
Farzane Nazmabadi <1379farzanenazmabadi@gmail.com>
2024-03-27 00:19:02 +0430

    creat header

---
+++ script/script.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    // autoplay:{
+    //     delay:2800;
+    // },
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    }
+  });
