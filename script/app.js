f1d6e3cf5275320d8740b6a8e639d256f8a97e4a
Farzane Nazmabadi <1379farzanenazmabadi@gmail.com>
2024-03-29 22:01:19 +0430

    Continue to complete the project and complete the desktop mode

---
+++ script/app.js
@@ -0,0 +1,39 @@
+let $=document;
+
+// Variables
+let users=[
+    {id:1,name:"farzane-nazmabadi",number:"9100038947#farzane"},
+    {id:2,name:"armin-amirinasab",number:"9904643198#armin"}
+];
+let usernameValue=$.querySelector(".username-input");
+let passwordValue=$.querySelector(".password-input");
+let submitBtn=$.querySelector(".button");
+let urlWrapper=$.querySelector(".show-url");
+let fileLink=$.querySelector("file-link");
+
+//Functions :
+
+// Check the Values of inputs.
+function checkInputValue(event){
+    event.preventDefault();
+    let username=usernameValue.value;
+    let password=passwordValue.value;
+    let availableValue=users.some(function(user){
+        return user.name===username && user.number===password;
+    });
+
+    if(availableValue){
+        urlWrapper.style.display="flex";
+    };
+    emptyValue(username,password);
+};
+
+//If input was not True.
+function emptyValue(user,pass){
+    if(user=="" || pass==""){
+        alert("لطفا نام کاربری و رمز عبور را به درستی وارد کنید !");
+    };
+};
+
+// Events
+submitBtn.addEventListener("click",checkInputValue);
--- script/script.js
+++ script/script.js
@@ -1,58 +1,81 @@
 let $=document;
-let navMenu=$.querySelector(".mainmenu")
-let menuItem1=$.querySelector(".item1")
-let menuItem2=$.querySelector(".item2")
-let menuItem3=$.querySelector(".item3")
-let menuItem4=$.querySelector(".item4")
+
+// Variables
+let navMenu=$.querySelector(".mainmenu");
+let menuItem1=$.querySelector(".item1");
+let menuItem2=$.querySelector(".item2");
+let menuItem3=$.querySelector(".item3");
+let menuItem4=$.querySelector(".item4");
+let backToTop=$.querySelector(".back-to-top");
+
+// Functions : 
+
+// This function is responsible for making the menu bar stick when the page is scrolled.
 function scrollNavMenu(){
-  if($.documentElement.scrollTop>400){
+  if($.documentElement.scrollTop>330){
     navMenu.style.cssText="position:fixed; top:0;  padding:3rem 1rem 2rem 0; backdrop-filter: blur(10px); box-shadow:0 0 15px var(--secondry-blue); border-radius: 2rem;";
-  }
-  else{
-    navMenu.style.cssText="position:reletives; background-color: var(--body-color); padding:2rem 0;"
-  }
+  }else{
+    navMenu.style.cssText="position:reletives; background-color: transparent; padding:3rem 1rem 2rem 0;";
+  };
+};
 
-}
+// This function has the task of specifying the desired item of that section in the menu bar when the page reaches the desired section.
 function activeItemMenu(){
-  console.log($.documentElement.scrollTop)
-  if($.documentElement.scrollTop<400){
-    menuItem1.classList.remove("active")
-    menuItem2.classList.remove("active")
-    menuItem3.classList.remove("active")
-    menuItem4.classList.remove("active")
-  }
+  console.log($.documentElement.scrollTop);
+  if($.documentElement.scrollTop<330){
+    menuItem1.classList.remove("active");
+    menuItem2.classList.remove("active");
+    menuItem3.classList.remove("active");
+    menuItem4.classList.remove("active");
+  }else if($.documentElement.scrollTop>330 && $.documentElement.scrollTop<1350){
+    menuItem1.classList.add("active");
+    menuItem2.classList.remove("active");
+    menuItem3.classList.remove("active");
+    menuItem4.classList.remove("active");
+  }else if($.documentElement.scrollTop>1350 && $.documentElement.scrollTop<2200){
+    menuItem2.classList.add("active");
+    menuItem1.classList.remove("active");
+    menuItem3.classList.remove("active");
+    menuItem4.classList.remove("active");
+  }else if($.documentElement.scrollTop>2200 && $.documentElement.scrollTop<2550){
+    menuItem3.classList.add("active");
+    menuItem1.classList.remove("active");
+    menuItem2.classList.remove("active");
+    menuItem4.classList.remove("active");
+  }else if($.documentElement.scrollTop>2500){
+    menuItem4.classList.add("active");
+    menuItem1.classList.remove("active");
+    menuItem2.classList.remove("active");
+    menuItem3.classList.remove("active");
+  };
+};
+
+// This function is responsible for displaying the back to the top circle.
+function showBackToTopCircle(){
   if($.documentElement.scrollTop>400){
-    menuItem1.classList.add("active")
-    menuItem2.classList.remove("active")
-    menuItem3.classList.remove("active")
-    menuItem4.classList.remove("active")
-  }
-  if($.documentElement.scrollTop>1204){
-    menuItem2.classList.add("active")
-    menuItem1.classList.remove("active")
-    menuItem3.classList.remove("active")
-    menuItem4.classList.remove("active")
-  }
-  if($.documentElement.scrollTop>2000){
-    menuItem3.classList.add("active")
-    menuItem1.classList.remove("active")
-    menuItem2.classList.remove("active")
-    menuItem4.classList.remove("active")
-  }
-  if($.documentElement.scrollTop>2400){
-    menuItem4.classList.add("active")
-    menuItem1.classList.remove("active")
-    menuItem2.classList.remove("active")
-    menuItem3.classList.remove("active")
-  }
-}
+    backToTop.style.display="flex";
+  }else{
+    backToTop.style.display="none";
+  };
+};
+
+// This function is responsible for controlling the functions related to the scroll event.
 function scrollBar(){
-  scrollNavMenu()
-  activeItemMenu()
-}
+  scrollNavMenu();
+  activeItemMenu();
+  showBackToTopCircle();
+};
 
+// return to the top of the page.
+function backToTopHandeler(){
+  window.scrollTo(0,0);
+};
 
-window.addEventListener("scroll",scrollBar)
+// Events
+window.addEventListener("scroll",scrollBar);
+backToTop.addEventListener("click",backToTopHandeler);
+
+// Swiper Library
 var swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
